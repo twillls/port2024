4427b766285d794dfc5a4e5ea61a8ad3594cd019
Taylor Wills <taylorwills@Taylors-MacBook-Pro.local>
2024-04-22 15:54:41 -0400

    Update gitignore, add base colors/variables, update gulpfile

--- web/themes/custom/taylorwills/gulpfile.js
+++ web/themes/custom/taylorwills/gulpfile.js
@@ -1,4 +1,6 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const minify = require('gulp-minify');
 const rename = require('gulp-rename');
 const concat = require('gulp-concat');
 const cleanCss = require('gulp-clean-css');
@@ -6,9 +8,15 @@ const cleanCss = require('gulp-clean-css');
 // Concat and Minify
 gulp.task('build-css', () => {
   return gulp.src('src/*.scss')
+  .pipe(sass())
   .pipe(concat('app.css'))
   .pipe(cleanCss())
   .pipe(gulp.dest('css/'))
+  .pipe(minify({
+    ext: '.css',
+    mangle: false,
+    noSource: true
+  }))
   .pipe(rename({ extname: 'min.js' }))
 });
 
@@ -17,3 +25,7 @@ gulp.task('session-start', (cb) => {
 });
 
 gulp.task('default', gulp.series('session-start'));
+
+gulp.task('watch', function() {
+  gulp.watch('src/*.scss', gulp.series('build-css'));
+});
